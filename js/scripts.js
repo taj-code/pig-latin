b33edc4b46e3b6a12779edddd357b626a287db37
taj-code <tajofisher4@gmail.com>
2021-08-18 15:28:33 -0700

    Co-authored-by: Chynna Lew <chynnalew@yahoo.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,50 @@
-$(document).ready(function() {
-  const vowels = ["a" , "e" , "i" , "o" , "u"];
-  const consonants = ["b" , "c" , "d" , "f" , "g" , "h" , "j" , "k" , "l" , "m" , "n" , "p" , "q" , "r" , "s" , "t" , "v" , "w" , "x" , "y" , "z"]
-  function pigLatin(userInput) {
-    let inputArray = userInput.toLowerCase().split("");
-    for (i=0; i<inputArray.length; i++) {
-      for(j=0; j<vowels.length; j++) {
-        for (h=0; )
-        if (inputArray[0] === vowels[j]) {
-          return userInput.concat("way"); 
-        } if else (inputArray[i] === consonants[]{
-          return false;
-        }
-      }
+
+const vowels = ["a" , "e" , "i" , "o" , "u"];
+const consonants = ["b" , "c" , "d" , "f" , "g" , "h" , "j" , "k" , "l" , "m" , "n" , "p" , "q" , "r" , "s" , "t" , "v" , "w" , "x" , "y" , "z"]
+
+function firstVowel(word) {
+  for (i=0; i<vowels.length; i++) {
+    if (word.charAt(0) === vowels[i]) {
+      return true;
     }
+    return false;
   }
-})
+}
+function recCons(word) {
+  for (i=0; i<consonants.length; i++) {
+    if (word.charAt(0) === consonants[i]){
+      return true;
+    }
+    return false;
+  }
+}
+
+
+
+function pigLatin(text){
+  if (firstVowel(word).valueOf() === true) {
+    return word.concat("way");
+  } else if ()
+}
+
+
+  // function pigLatin(userInput) {
+  //   let inputString = userInput.toLowerCase();
+  //   for (i=0; i<inputString.length; i++) {
+  //     for(j=0; j<vowels.length; j++) {
+  //       for (h=0; h<consonants.length; h++)
+  //       if (inputString[0] === vowels[j]) {
+  //         return userInput.concat("way"); 
+  //       } else if (inputString[i] === consonants[h]) {
+  //         let wordCons = [];
+  //         wordCons.push(inputString[i]);
+  //         return wordCons;
+  //       }
+  //     }
+  //   }
+  // }
+
+
+
 
 // let userInput = $("#userInput").val();
